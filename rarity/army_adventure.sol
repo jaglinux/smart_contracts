5430d45efee632482d0ef3d04b7cfc9c9d2fb625
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2021-10-04 23:44:21 -0700

    Update army_adventure.sol

--- rarity/army_adventure.sol
+++ rarity/army_adventure.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.7;
 interface IRarity {
     function adventure(uint _summoner) external;
     function ownerOf(uint256 tokenId) external view returns (address owner);
+    function adventurers_log(uint256 tokenId) external view returns (uint256);
 }
 
 contract army_adventure {
@@ -17,14 +18,23 @@ contract army_adventure {
     function register_to_army(uint256[] calldata _summoners) external {
         uint256 len = _summoners.length;
         for (uint256 i = 0; i < len; i++) {
+            // allowing owners only to add their summoners
             require(msg.sender == rarity.ownerOf(_summoners[i]), "only owner can train");
             army[msg.sender].push(_summoners[i]);
         }
     }
     
-    function command_adventure_all(address _commander) external{
+    function command_adventure_all() external{
+        uint256 len = army[msg.sender].length;
+        for(uint256 i = 0; i < len; i++) {
+            rarity.adventure(army[msg.sender][i]);
+        }
+    }
+    
+    function command_adventure_all_overload(address _commander) external{
         uint256 len = army[_commander].length;
         for(uint256 i = 0; i < len; i++) {
+            //allowing anyone to order adventure, why would anyone do so ?!
             rarity.adventure(army[_commander][i]);
         }
     }
