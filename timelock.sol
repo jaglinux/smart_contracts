4f77ff4a18053d238c57e28b3090ce45aaf95c97
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-09-29 01:08:56 -0700

    Update timelock.sol

--- timelock.sol
+++ timelock.sol
@@ -1,27 +1,74 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
 
-pragma solidity >=0.7.0 <0.8.0;
+contract Timelock {
+    error ProposalListed();
+    error ProposalTSError();
+    error ProposalExecuted();
+    error OnlyOwner();
+    error ProposalNotListed();
+    error ProposalTimeExpired();
+    error ExecutionFailed();
 
-contract timelock {
-    uint256 public origin_block;
-    uint256 public origin_timestamp;
-    uint256 a;
-    address payable public owner;
-    
-    constructor() payable {
-        origin_block = block.number;
-        origin_timestamp = block.timestamp;
+    address public owner;
+    uint256 public constant MIN_DELAY = 10 seconds;
+    uint256 public constant MAX_DELAY = 1000 seconds;
+    uint256 public constant GRACE_PERIOD = 1000 seconds;
+
+
+    struct Proposal {
+        bool listed;
+        bool executed;
+    }
+
+    mapping(bytes32 => Proposal) public Proposals;
+
+    constructor() {
         owner = msg.sender;
     }
-    
-    //for local test network, call dummy to mine the block !
-    function dummy() public {
-        a++;
+
+    modifier onlyOwner() {
+        if(msg.sender != owner) revert OnlyOwner();
+        _;
+    }
+
+    function getBlockTimeStamp() external view returns(uint256) {
+        return block.timestamp;
+    }
+
+    function QueueProposal(address _addr, bytes4 _func, bytes memory _param, uint256 _ts) 
+        external onlyOwner returns(bytes32 _hash) {
+            _hash = getHash(_addr, _func, _param, _ts);
+            if(Proposals[_hash].listed == true) 
+                revert ProposalListed();
+            if(Proposals[_hash].executed == true)
+                revert ProposalExecuted();
+            if(_ts < (block.timestamp+MIN_DELAY) || _ts > (block.timestamp+MAX_DELAY)) 
+                revert ProposalTSError();
+            Proposals[_hash].listed = true;
+    }
+
+    function ExecuteProposal(address _addr, bytes4 _func, bytes memory _param, uint256 _ts) 
+        external {
+        bytes32 _hash = getHash(_addr, _func, _param, _ts);
+        if(Proposals[_hash].listed == false) 
+            revert ProposalNotListed();
+        if(Proposals[_hash].executed == true)
+            revert ProposalExecuted();
+        if(block.timestamp > (_ts+GRACE_PERIOD))
+            revert ProposalTimeExpired();
+        (bool success, bytes memory log) = _addr.call(abi.encode(_func, _param));
+        if(success) {
+            Proposals[_hash].executed = true;
+        }
+        else {
+            revert ExecutionFailed();
+        }
     }
     
-    function withdraw() external {
-        require(msg.sender == owner);
-        require(block.number >= origin_block + 5);
-        owner.transfer(address(this).balance);
+    function getHash(address _addr, bytes4 _func, bytes memory _param, uint256 _ts) internal pure 
+        returns(bytes32) {
+            return keccak256(abi.encodePacked(_addr, _func, _param, _ts));
     }
+
 }
