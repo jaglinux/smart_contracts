24ff4ffd8d5cad5618976e50015a9d432986619b
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-10-01 01:02:43 -0700

    Update D_NFT.sol

--- dynamicNFT/D_NFT.sol
+++ dynamicNFT/D_NFT.sol
@@ -12,6 +12,8 @@ contract D_NFT is ERC721 {
     uint256 constant maxSupply = 10;
 
     error MintOver();
+    
+    //Set _base param to https://gateway.pinata.cloud/ipfs/QmU8AZ8wAeNwEfstLkE91FEArPnu9v9BZZegK1SnHcsVnL
     constructor(string memory _name, string memory _symbol, string memory _base) 
         ERC721(_name, _symbol) {
             baseURL = _base;
