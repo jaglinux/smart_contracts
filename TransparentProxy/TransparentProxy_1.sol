5f14e99452c0f379d979d83a8cc8413d28404719
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-10-10 00:09:37 -0700

    Create TransparentProxy_1.sol

---
+++ TransparentProxy/TransparentProxy_1.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+contract TransparentProxy {
+
+    address public owner;
+    address public impl;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "onlyOwner");
+        _;
+    }
+
+    function upgrade(address _impl) external onlyOwner() {
+        impl = _impl;
+    }
+
+    function delegate(address _impl) private {
+            assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch pad at memory position 0.
+
+            // calldatacopy(t, f, s) - copy s bytes from calldata at position f to mem at position t
+            // calldatasize() - size of call data in bytes
+            calldatacopy(0, 0, calldatasize())
+
+            // Call the implementation.
+            // out and outsize are 0 because we don't know the size yet.
+
+            // delegatecall(g, a, in, insize, out, outsize) -
+            // - call contract at address a
+            // - with input mem[in…(in+insize))
+            // - providing g gas
+            // - and output area mem[out…(out+outsize))
+            // - returning 0 on error (eg. out of gas) and 1 on success
+            let result := delegatecall(gas(), _impl, 0, calldatasize(), 0, 0)
+
+            // Copy the returned data.
+            // returndatacopy(t, f, s) - copy s bytes from returndata at position f to mem at position t
+            // returndatasize() - size of the last returndata
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+            // delegatecall returns 0 on error.
+            case 0 {
+                // revert(p, s) - end execution, revert state changes, return data mem[p…(p+s))
+                revert(0, returndatasize())
+            }
+            default {
+                // return(p, s) - end execution, return data mem[p…(p+s))
+                return(0, returndatasize())
+            }
+        }
+    }
+
+    receive() external payable {
+        delegate(impl);
+    }
+
+    fallback() external payable {
+        delegate(impl);
+    }
+}
